cf36929a588490c160461b4bfe588adf9fb66afe
dmason30 <danmason@spendology.com>
2023-01-30 15:18:58 +0000

    Handle when assets are registered in a tool boot method

---
+++ tests/Support/TestTool.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Fidum\NovaPackageBundler\Tests\Support;
+
+use Illuminate\Http\Request;
+use Laravel\Nova\Nova;
+use Laravel\Nova\Tool;
+use function Pest\testDirectory;
+
+class TestTool extends Tool
+{
+    public function boot()
+    {
+        Nova::script('tool-boot-package', testDirectory('fixtures/input/tool/tool.js'));
+        Nova::style('tool-boot-package', testDirectory('fixtures/input/tool/tool.css'));
+    }
+
+    public function menu(Request $request)
+    {
+        // Should never be called
+    }
+}
